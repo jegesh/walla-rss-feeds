9179247f8dce883b05f5c63bea96cdee9d3801fc
Yaakov Gesher <yaakov@gesher.net>
2016-08-10 18:26:08 +0300

    initial

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rssAppApp
+ * @description
+ * # rssAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rssAppApp', [
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rssAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rssAppApp
+ */
+angular.module('rssAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
